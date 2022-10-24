7ee435cb40919f6596b5d2d7d6cc74b6234e33d2
Filimonov-d <filimonov.d.ua@gmail.com>
2022-10-24 20:07:25 +0200

    add feature MinMax arr

--- main.go
+++ main.go
@@ -5,21 +5,10 @@ import (
 	//"github.com/gin-gonic/gin"
 )
 
-func Max(x ...int) {
-
-	var max = 0
-
-	for _, number := range x {
-		if number > max {
-			max = number
-		}
-	}
-	fmt.Println("max num: ", max)
-}
-
-func Min(y ...int) {
+func MinMax(y []int, z []int) {
 
 	min := 999999999999999999
+	var max = 0
 
 	for _, number := range y {
 		if number < min {
@@ -27,11 +16,17 @@ func Min(y ...int) {
 		}
 	}
 	fmt.Println("min num: ", min)
+
+	for _, number := range z {
+		if number > max {
+			max = number
+		}
+	}
+	fmt.Println("max num: ", max)
+
 }
 
 func main() {
 
-	Min([]int{8, 12, 56, 4, 3}...)
-	Max([]int{8, 12, 56, 4, 3}...)
-
+	MinMax([]int{8, 12, 56, 4, 3}, []int{8, 12, 56, 4, 3})
 }
